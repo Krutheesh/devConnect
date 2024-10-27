7f66492733cec20d1cca876dcda0bc6375fd44f4
krutheesh <krutheeshsiripuram@gmail.com>
2024-10-27 21:41:04 +0530

    same site

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -67,6 +67,7 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
+     sameSite:"None"
   });
   res.send("Logout Successful!!");
 });
