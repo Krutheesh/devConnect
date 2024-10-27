7e81693e03195370b1967c03ef39cd9f071cf211
krutheesh <krutheeshsiripuram@gmail.com>
2024-10-27 21:46:28 +0530

    same site

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,7 +29,8 @@ authRouter.post("/signup", async (req, res) => {
 
     res.cookie("token", token, {
       expires: new Date(Date.now() + 8 * 3600000),
-      sameSite:"None"
+      sameSite:"None",
+     secure: true
      
     });
 
@@ -54,6 +55,8 @@ authRouter.post("/login", async (req, res) => {
 
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
+        sameSite:"None",
+        secure: true
       });
       res.send(user);
     } else {
@@ -67,7 +70,8 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
-     sameSite:"None"
+     sameSite:"None",
+     secure: true
   });
   res.send("Logout Successful!!");
 });
